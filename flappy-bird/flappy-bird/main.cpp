14f1367a01d29e4675ef2d50fc2622c82c879a38
kai <zk.zhengkai@outlook.com>
2020-04-14 12:49:30 -0400

    basic setting

---
+++ flappy-bird/flappy-bird/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  flappy-bird
+//
+//  Created by Kai Zheng on 4/14/20.
+//  Copyright © 2020 kai. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
