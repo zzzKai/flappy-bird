3b24fe62b90402b7db72e935d9f1119965e2e860
kai <zk.zhengkai@outlook.com>
2020-04-14 12:52:38 -0400

    window setting

--- flappy-bird/flappy-bird/main.cpp
+++ flappy-bird/flappy-bird/main.cpp
@@ -6,10 +6,33 @@
 //  Copyright © 2020 kai. All rights reserved.
 //
 
+#include <SFML/Graphics.hpp>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+#define SCREEN_WIDTH 1024
+#define SCREEN_HEIGHT 1024
+
+int main() {
+    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Flappy Bird");
+    
+    while (window.isOpen()) {
+        // handle input/events
+        sf::Event event;
+        
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+            }
+        }
+        
+        // update game logic
+        
+        window.clear();
+        
+        // draw objects
+        window.display();
+    }
+    return EXIT_SUCCESS;
 }
