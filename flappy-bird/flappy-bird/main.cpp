5839eea0c801b7536514fa6b03f22ee9993f661f
kai <zk.zhengkai@outlook.com>
2020-04-15 02:35:57 -0400

    delete test code in the main

--- flappy-bird/flappy-bird/main.cpp
+++ flappy-bird/flappy-bird/main.cpp
@@ -6,33 +6,9 @@
 //  Copyright © 2020 kai. All rights reserved.
 //
 
-#include <SFML/Graphics.hpp>
 #include <iostream>
 
-#define SCREEN_WIDTH 1024
-#define SCREEN_HEIGHT 1024
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(SCREEN_WIDTH, SCREEN_HEIGHT), "Flappy Bird");
-    
-    while (window.isOpen()) {
-        // handle input/events
-        sf::Event event;
-        
-        while (window.pollEvent(event)) {
-            switch (event.type) {
-                case sf::Event::Closed:
-                    window.close();
-                    break;
-            }
-        }
-        
-        // update game logic
-        
-        window.clear();
-        
-        // draw objects
-        window.display();
-    }
     return EXIT_SUCCESS;
 }
