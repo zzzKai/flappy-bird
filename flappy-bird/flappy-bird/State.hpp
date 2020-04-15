ea63043f41ef4796e6d01afb54ee196a6c068131
kai <zk.zhengkai@outlook.com>
2020-04-14 20:18:36 -0400

    state header file

---
+++ flappy-bird/flappy-bird/State.hpp
@@ -0,0 +1,23 @@
+//
+//  State.hpp
+//  flappy-bird
+//
+//  Created by Kai Zheng on 4/14/20.
+//  Copyright © 2020 kai. All rights reserved.
+//
+
+#pragma once
+
+namespace Kai {
+    class State {
+        public:
+        virtual void Init() = 0;
+        
+        virtual void HandleInput() = 0;
+        virtual void Update() = 0;
+        virtual void Draw(float dt) = 0;
+        
+        virtual void Pause() {}
+        virtual void Resume() {}
+    };
+}
