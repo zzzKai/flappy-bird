2e91e5272bf46c55dd636aa05dfc8ce1f2543327
kai <zk.zhengkai@outlook.com>
2020-04-15 12:58:05 -0400

    minor changes

--- flappy-bird/flappy-bird/StateMachine.cpp
+++ flappy-bird/flappy-bird/StateMachine.cpp
@@ -10,42 +10,42 @@
 
 namespace Kai {
     void StateMachine::AddState(StateRef newState, bool isReplacing) {
-        this -> _isAdding = true;
-        this -> _isReplacing = isReplacing;
+        this->_isAdding = true;
+        this->_isReplacing = isReplacing;
         
-        this -> _newState = std::move(newState);
+        this->_newState = std::move(newState);
     }
 
     void StateMachine::RemoveState() {
-        this -> _isRemoving = true;
+        this->_isRemoving = true;
     }
 
     void StateMachine::ProcessStateChanges() {
-        if (this -> _isRemoving && !this -> _states.empty()) {
-            this -> _states.pop();
+        if (this->_isRemoving && !this->_states.empty()) {
+            this->_states.pop();
             
-            if (!this -> _states.empty()) {
-                this -> _states.top() -> Resume();
+            if (!this->_states.empty()) {
+                this->_states.top()->Resume();
             }
-            this -> _isRemoving = false;
+            this->_isRemoving = false;
         }
         
-        if (this -> _isAdding) {
-            if (!this -> _states.empty()) {
-                if (this -> _isReplacing) {
-                    this -> _states.pop();
+        if (this->_isAdding) {
+            if (!this->_states.empty()) {
+                if (this->_isReplacing) {
+                    this->_states.pop();
                 } else {
-                    this -> _states.top() -> Pause();
+                    this->_states.top()->Pause();
                 }
             }
             
-            this -> _states.push(std::move(this -> _newState));
-            this -> _states.top() -> Init();
-            this -> _isAdding = false;
+            this->_states.push(std::move(this ->_newState));
+            this->_states.top()->Init();
+            this->_isAdding = false;
         }
     }
 
     StateRef &StateMachine::GetActiveState() {
-        return this -> _states.top();
+        return this->_states.top();
     }
 }
