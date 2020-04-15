786f60b4739c8596e37611dc4e9ff6cbe77c8ba7
kai <zk.zhengkai@outlook.com>
2020-04-14 20:26:25 -0400

    State file

--- flappy-bird/flappy-bird/State.hpp
+++ flappy-bird/flappy-bird/State.hpp
@@ -10,9 +10,9 @@
 
 namespace Kai {
     class State {
-        public:
+    public:
         virtual void Init() = 0;
-        
+       
         virtual void HandleInput() = 0;
         virtual void Update() = 0;
         virtual void Draw(float dt) = 0;
