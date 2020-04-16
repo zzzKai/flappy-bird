9fda3342c9e16904111adcbd8579951bfc7eae74
kai <zk.zhengkai@outlook.com>
2020-04-16 03:43:02 -0400

    revise Update() input to float dt

--- flappy-bird/flappy-bird/State.hpp
+++ flappy-bird/flappy-bird/State.hpp
@@ -14,7 +14,7 @@ namespace Kai {
         virtual void Init() = 0;
        
         virtual void HandleInput() = 0;
-        virtual void Update() = 0;
+        virtual void Update(float dt) = 0;
         virtual void Draw(float dt) = 0;
         
         virtual void Pause() {}
