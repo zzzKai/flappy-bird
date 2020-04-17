21312084cdcbace39db44f4ba603f6fe993b5990
kai <zk.zhengkai@outlook.com>
2020-04-17 02:05:42 -0400

    add DEFINITION file

---
+++ flappy-bird/flappy-bird/DEFINITIONS.hpp
@@ -0,0 +1,16 @@
+//
+//  DEFINITIONS.hpp
+//  flappy-bird
+//
+//  Created by Kai Zheng on 4/17/20.
+//  Copyright © 2020 kai. All rights reserved.
+//
+
+#pragma once
+
+#define SCREEN_WIDTH 768
+#define SCREEN_HEIGHT 1024
+
+#define SPLASH_STATE_SHOW_TIME 3.0
+
+#define SPLASH_SCENE_BACKGROUND_FILEPATH "Resources/res/SplashBackground.png"
