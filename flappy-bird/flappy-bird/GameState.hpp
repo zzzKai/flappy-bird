50b9e4a9f4caf568e07e9750ca5fe1437ecbc4cb
kai <zk.zhengkai@outlook.com>
2020-04-21 23:19:58 -0400

    add game state header file

---
+++ flappy-bird/flappy-bird/GameState.hpp
@@ -0,0 +1,31 @@
+//
+//  GameState.hpp
+//  flappy-bird
+//
+//  Created by Kai Zheng on 4/21/20.
+//  Copyright © 2020 kai. All rights reserved.
+//
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "State.hpp"
+#include "Game.hpp"
+
+namespace Kai {
+    class GameState : public State {
+        public:
+            GameState(GameDataRef data);
+            
+            void Init();
+            
+            void HandleInput();
+            void Update(float dt);
+            void Draw(float dt);
+            
+        private:
+            GameDataRef _data;
+            
+            sf::Sprite _background;
+    };
+}
