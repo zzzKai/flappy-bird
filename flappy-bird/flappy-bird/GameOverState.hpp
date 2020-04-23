1d19af8cb31499afd6aae920463a1b5ab0aa7e30
kai <zk.zhengkai@outlook.com>
2020-04-22 23:21:03 -0400

    add GameOverState header file

---
+++ flappy-bird/flappy-bird/GameOverState.hpp
@@ -0,0 +1,31 @@
+//
+//  GameOverState.hpp
+//  flappy-bird
+//
+//  Created by Kai Zheng on 4/22/20.
+//  Copyright © 2020 kai. All rights reserved.
+//
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "State.hpp"
+#include "Game.hpp"
+
+namespace Kai {
+    class GameOverState : public State {
+        public:
+            GameOverState(GameDataRef data);
+            
+            void Init();
+            
+            void HandleInput();
+            void Update(float dt);
+            void Draw(float dt);
+            
+        private:
+            GameDataRef _data;
+            
+            sf::Sprite _background;
+    };
+}
