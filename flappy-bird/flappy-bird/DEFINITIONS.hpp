7f10850b462e5c5790a5d3cfc9960a66ce68d23d
kai <zk.zhengkai@outlook.com>
2020-04-22 23:20:45 -0400

    add game over back ground file path

--- flappy-bird/flappy-bird/DEFINITIONS.hpp
+++ flappy-bird/flappy-bird/DEFINITIONS.hpp
@@ -16,6 +16,7 @@
 #define SPLASH_SCENE_BACKGROUND_FILEPATH "Resources/res/Splash Background.png"
 #define MAIN_MENU_BACKGROUND_FILEPATH "Resources/res/sky.png"
 #define GAME_BACKGROUND_FILEPATH "Resources/res/sky.png"
+#define GAME_OVER_BACKGROUND_FILEPATH "Resources/res/sky.png"
 
 #define GAME_TITLE_FILEPATH "Resources/res/title.png"
 #define PLAY_BUTTON_FILEPATH "Resources/res/PlayButton.png"
