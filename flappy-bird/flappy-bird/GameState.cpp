06041820bd1e329a15f4c0cccc6b48e0faaab762
kai <zk.zhengkai@outlook.com>
2020-04-24 21:19:08 -0400

    add move pipe to game state

--- flappy-bird/flappy-bird/GameState.cpp
+++ flappy-bird/flappy-bird/GameState.cpp
@@ -38,11 +38,17 @@ namespace Kai {
             if (sf::Event::Closed == event.type) {
                 _data->window.close();
             }
+            
+            if (_data->input.IsSpiteClicked(_background, sf::Mouse::Left, _data->window)) {
+                pipe->SpawnInvisiblePipe();
+                pipe->SpawnBottomPipe();
+                pipe->SpawnTopPipe();
+            }
         }
     }
     
     void GameState::Update(float dt) {
-        
+        pipe->MovePipes(dt);
     }
     
     void GameState::Draw(float dt) {
