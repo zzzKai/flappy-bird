df381fdd536d42656aba097c5d7121e08a6825de
kai <zk.zhengkai@outlook.com>
2020-05-05 22:43:26 -0400

    create land header file

---
+++ flappy-bird/flappy-bird/Land.hpp
@@ -0,0 +1,27 @@
+//
+//  Land.hpp
+//  flappy-bird
+//
+//  Created by Kai Zheng on 5/5/20.
+//  Copyright © 2020 kai. All rights reserved.
+//
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Game.hpp"
+#include <vector>
+
+namespace Kai {
+    class Land {
+        public:
+            Land(GameDataRef data);
+            
+            void MoveLand(float dt);
+            void DrawLand();
+            
+        private:
+            GameDataRef _data;
+            std::vector<sf::Sprite> _landSprites;
+    };
+}
