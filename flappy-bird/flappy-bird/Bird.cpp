7b55e71f0921c9b8704c174513ed53a7989619e3
kai <zk.zhengkai@outlook.com>
2020-05-10 23:41:30 -0400

    implement Animate()

--- flappy-bird/flappy-bird/Bird.cpp
+++ flappy-bird/flappy-bird/Bird.cpp
@@ -9,11 +9,32 @@
 #include "Bird.hpp"
 
 namespace Kai {
-    Bird::Bird(GameDataRef data) : _data(data) {
-        _birdSprite.setTexture(_data->assets.GetTexture("Bird Frame 1"));
+    Bird::Bird(GameDataRef data) : _data(data) {        
+        _animationIterator = 0;
+        
+        _animationFrames.push_back(_data->assets.GetTexture("Bird Frame 1"));
+        _animationFrames.push_back(_data->assets.GetTexture("Bird Frame 2"));
+        _animationFrames.push_back(_data->assets.GetTexture("Bird Frame 3"));
+        _animationFrames.push_back(_data->assets.GetTexture("Bird Frame 4"));
+        
+        _birdSprite.setTexture(_animationFrames.at(_animationIterator));
     }
     
     void Bird::Draw() {
         _data->window.draw(_birdSprite);
+    }
+    
+    void Bird::Animate(float dt) {
+        if (_clock.getElapsedTime().asSeconds() > BIRD_ANIMATION_DURATION / _animationFrames.size()) {
+            if (_animationIterator < _animationFrames.size() - 1) {
+                _animationIterator++;
+            } else {
+                _animationIterator = 0;
+            }
+            
+            _birdSprite.setTexture(_animationFrames.at(_animationIterator));
+            
+            _clock.restart();
+        }    
     }
 }
