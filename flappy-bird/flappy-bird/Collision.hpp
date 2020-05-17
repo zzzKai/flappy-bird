c82091bde7ca2ed145f16052afd5ca4353c4f147
kai <zk.zhengkai@outlook.com>
2020-05-17 02:25:39 -0400

    overload CheckSpriteCollision with scaling variable

--- flappy-bird/flappy-bird/Collision.hpp
+++ flappy-bird/flappy-bird/Collision.hpp
@@ -16,5 +16,7 @@ namespace Kai {
             Collision();
             
             bool CheckSpriteCollision(sf::Sprite sprite1, sf::Sprite sprite2);
+            
+            bool CheckSpriteCollision(sf::Sprite sprite1, float scale1, sf::Sprite sprite2, float scale2);
     };
 }
