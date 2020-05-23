f866cf8346161d33375e23040be3b33fea24d3c1
kai <zk.zhengkai@outlook.com>
2020-05-23 01:56:05 -0400

    add definition for game over set up

--- flappy-bird/flappy-bird/DEFINITIONS.hpp
+++ flappy-bird/flappy-bird/DEFINITIONS.hpp
@@ -34,6 +34,9 @@
 
 #define FLAPPY_FONT_FILEPATH "Resources/fonts/FlappyFont.ttf"
 
+#define GAME_OVER_TITLE_FILEPATH "Resources/res/Game-Over-Title.png"
+#define GAME_OVER_BODY_FILEPATH "Resources/res/Game-Over-Body.png"
+
 #define PIPE_MOVEMENT_SPEED 200.0f
 #define PIPE_SPAWN_FREQUENCY 2.5f
 
@@ -57,3 +60,5 @@ enum GameStates {
 };
 
 #define FLASH_SPEED 1500.0f
+
+#define TIME_BEFORE_GAME_OVER_APPEARS 1.5f
