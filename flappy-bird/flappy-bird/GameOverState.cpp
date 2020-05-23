176cb720d163810f0810d583bf794b339a645d9d
kai <zk.zhengkai@outlook.com>
2020-05-23 02:13:03 -0400

    add game over title, game over container, retry button

--- flappy-bird/flappy-bird/GameOverState.cpp
+++ flappy-bird/flappy-bird/GameOverState.cpp
@@ -10,6 +10,7 @@
 #include <iostream>
 #include "GameOverState.hpp"
 #include "DEFINITIONS.hpp"
+#include "GameState.hpp"
 
 
 
@@ -22,8 +23,19 @@ namespace Kai {
         
         
         _data->assets.LoadTexture("Game Over Background", GAME_OVER_BACKGROUND_FILEPATH);
+        _data->assets.LoadTexture("Game Over Title", GAME_TITLE_FILEPATH);
+        _data->assets.LoadTexture("Game Over Body", GAME_OVER_BODY_FILEPATH);
         
         _background.setTexture(this->_data->assets.GetTexture("Game Over Background"));
+        _gameOverTitle.setTexture(this->_data->assets.GetTexture("Game Over Title"));
+        _gameOverContainer.setTexture(this->_data->assets.GetTexture("Game Over Body"));
+        _retryButton.setTexture(this->_data->assets.GetTexture("Play Button"));
+        
+        _gameOverContainer.setPosition((_data->window.getSize().x / 2) - (_gameOverContainer.getGlobalBounds().width / 2), (_data->window.getSize().y / 2) - (_gameOverContainer.getGlobalBounds().height / 2));
+        
+        _gameOverTitle.setPosition((_data->window.getSize().x / 2) - (_gameOverTitle.getGlobalBounds().width / 2), _gameOverContainer.getPosition().y - (_gameOverTitle.getGlobalBounds().height * 1.2));
+        
+        _retryButton.setPosition((_data->window.getSize().x / 2) - (_retryButton.getGlobalBounds().width / 2), _gameOverContainer.getPosition().y + _gameOverContainer.getGlobalBounds().height + (_retryButton.getGlobalBounds().height * 0.2));
     }
     
     void GameOverState::HandleInput() {
@@ -32,6 +44,10 @@ namespace Kai {
         while (_data->window.pollEvent(event)) {
             if (sf::Event::Closed == event.type) {
                 _data->window.close();
+            }
+            
+            if (_data->input.IsSpiteClicked(_retryButton, sf::Mouse::Left, _data->window)) {
+                _data->machine.AddState(StateRef(new GameState(_data)), true);
             }
         }
     }
@@ -45,6 +61,10 @@ namespace Kai {
         _data->window.clear();
         
         _data->window.draw(_background);
+        _data->window.draw(_gameOverTitle);
+        _data->window.draw(_gameOverContainer);
+        _data->window.draw(_retryButton);
+        
         
         _data->window.display();
     }
--- flappy-bird/flappy-bird/GameOverState.hpp
+++ flappy-bird/flappy-bird/GameOverState.hpp
@@ -27,5 +27,9 @@ namespace Kai {
             GameDataRef _data;
             
             sf::Sprite _background;
+            
+            sf::Sprite _gameOverTitle;
+            sf::Sprite _gameOverContainer;
+            sf::Sprite _retryButton;
     };
 }
