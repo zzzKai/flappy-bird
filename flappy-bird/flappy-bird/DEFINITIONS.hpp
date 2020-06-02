c3ff40ec60d7ff9ef419333001f5a76dc456dac5
kai <zk.zhengkai@outlook.com>
2020-06-01 22:36:46 -0400

    add definition for medal

--- flappy-bird/flappy-bird/DEFINITIONS.hpp
+++ flappy-bird/flappy-bird/DEFINITIONS.hpp
@@ -37,6 +37,11 @@
 #define GAME_OVER_TITLE_FILEPATH "Resources/res/Game-Over-Title.png"
 #define GAME_OVER_BODY_FILEPATH "Resources/res/Game-Over-Body.png"
 
+#define BRONZE_MEDAL_FILEPATH "Resources/res/Bronze-Medal.png"
+#define SILVER_MEDAL_FILEPATH "Resources/res/Silver-Medal.png"
+#define GOLD_MEDAL_FILEPATH "Resources/res/Gold-Medal.png"
+#define PLATINUM_MEDAL_FILEPATH "Resources/res/Platinum-Medal.png"
+
 #define PIPE_MOVEMENT_SPEED 200.0f
 #define PIPE_SPAWN_FREQUENCY 2.5f
 
@@ -62,3 +67,8 @@ enum GameStates {
 #define FLASH_SPEED 1500.0f
 
 #define TIME_BEFORE_GAME_OVER_APPEARS 1.5f
+
+#define BRONZE_MEDAL_SCORE 0
+#define SILVER_MEDAL_SCORE 5
+#define GOLD_MEDAL_SCORE 25
+#define PLATINUM_MEDAL_SCORE 100
